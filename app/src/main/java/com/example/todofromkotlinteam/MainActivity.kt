6206aee9aacbf3dc776ec98afb65c2cc138331c8
VAmpersand <49166976+VAmpersand@users.noreply.github.com>
2021-08-04 19:51:42 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d("First example", "123324")
+        Log.d("First example", "Not first example")
     }
 }
