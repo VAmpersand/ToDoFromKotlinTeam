ed9d748c032012333b4ec9b53b5f799030666ff1
VAmpersand <VP_ampersand_P@icloud.com>
2021-08-18 19:15:26 +0300

    Fixed sharedPreferencesManager

--- app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.todofromkotlinteam
 
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import com.example.todofromkotlinteam.managers.SharedPreferencesKey
 import com.example.todofromkotlinteam.managers.SharedPreferencesManager
 
@@ -18,28 +15,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showWelcomeActivityIfNeeded() {
-//        val manager = SharedPreferencesManager()
-        val value = getBoolValueFor(SharedPreferencesKey.WelcomeActivityWasShown)
+        val manager = SharedPreferencesManager(this)
+        val value = manager.getBoolValueFor(SharedPreferencesKey.WelcomeActivityWasShown)
 
         if (value != true) {
             val intent = Intent(this, WelcomeActivity::class.java)
             startActivityForResult(intent, 2)
 
-           setBoolValueFor(SharedPreferencesKey.WelcomeActivityWasShown, true)
+            manager.setBoolValueFor(SharedPreferencesKey.WelcomeActivityWasShown, true)
         }
-    }
-
-    val title = "ToDoSharedPreferences"
-
-    fun setBoolValueFor(key: SharedPreferencesKey, value: Boolean) {
-        val pref: SharedPreferences = getSharedPreferences(title, Context.MODE_PRIVATE)
-        val editor = pref?.edit()
-        editor?.putBoolean(key.toString(), value)
-        editor?.apply()
-    }
-
-    fun getBoolValueFor(key: SharedPreferencesKey): Boolean? {
-        val pref: SharedPreferences = getSharedPreferences(title, Context.MODE_PRIVATE)
-        return pref?.getBoolean(key.toString(), false)
     }
 }
