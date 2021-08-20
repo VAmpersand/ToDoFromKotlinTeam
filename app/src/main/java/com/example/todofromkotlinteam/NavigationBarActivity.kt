ad9979268bc0e8fadd5c2c41a16c324dcf2f8181
VAmpersand <VP_ampersand_P@icloud.com>
2021-08-20 20:25:56 +0300

    Added navigation tabBar

---
+++ app/src/main/java/com/example/todofromkotlinteam/NavigationBarActivity.kt
@@ -0,0 +1,49 @@
+package com.example.todofromkotlinteam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.todofromkotlinteam.managers.SharedPreferencesKey
+import com.example.todofromkotlinteam.managers.SharedPreferencesManager
+import kotlinx.android.synthetic.main.navigation_bar_activity.*
+
+class NavigationBarActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.navigation_bar_activity)
+
+        showWelcomeActivityIfNeeded()
+        configureNavigationBar()
+    }
+
+    private fun showWelcomeActivityIfNeeded() {
+        val value = SharedPreferencesManager.getBoolValueFor(SharedPreferencesKey.WelcomeActivityWasShown, this)
+
+        if (value != true) {
+            val intent = Intent(this, WelcomeActivity::class.java)
+            startActivityForResult(intent, 2)
+            SharedPreferencesManager.setBoolValueFor(SharedPreferencesKey.WelcomeActivityWasShown, true, this)
+        }
+    }
+
+    private fun configureNavigationBar() {
+        navigationView.background = null
+        navigationView.menu.getItem(2).isEnabled = false
+
+        navigationView.setOnNavigationItemSelectedListener { item ->
+            var seletedFragment: Fragment? = null
+
+            when (item.itemId) {
+                R.id.plan -> seletedFragment = PlansFragment()
+                R.id.ideas -> seletedFragment = IdeasFragment()
+                R.id.profile -> seletedFragment = ProfileFragment()
+                R.id.settings -> seletedFragment = SettingsFragment(this)
+            }
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragmentContainerView, seletedFragment!!).commit()
+
+            true
+        }
+    }
+}
--- app/src/main/java/com/example/todofromkotlinteam/managers/SharedPreferencesManager.kt
+++ app/src/main/java/com/example/todofromkotlinteam/managers/SharedPreferencesManager.kt
@@ -1,32 +1,32 @@
 package com.example.todofromkotlinteam.managers
 
 import android.content.Context
-import android.content.SharedPreferences
-import android.view.View
 
 enum class SharedPreferencesKey {
     WelcomeActivityWasShown
 }
 
-class SharedPreferencesManager(context: Context) {
+object SharedPreferencesManager {
     private val title = "ToDoSharedPreferences"
-    private var preferences: SharedPreferences? = null
 
     init {
-        preferences = context.getSharedPreferences(title, Context.MODE_PRIVATE)
+        println("Singleton initialized")
     }
 
-    fun setBoolValueFor(key: SharedPreferencesKey, value: Boolean) {
+    fun setBoolValueFor(key: SharedPreferencesKey, value: Boolean, context: Context) {
+        val preferences = context.getSharedPreferences(title, Context.MODE_PRIVATE)
         val editor = preferences?.edit()
         editor?.putBoolean(key.toString(), value)
         editor?.apply()
     }
 
-    fun getBoolValueFor(key: SharedPreferencesKey): Boolean? {
+    fun getBoolValueFor(key: SharedPreferencesKey, context: Context): Boolean? {
+        val preferences = context.getSharedPreferences(title, Context.MODE_PRIVATE)
         return preferences?.getBoolean(key.toString(), false)
     }
 
-    fun resetSharedPreferences(){
+    fun resetSharedPreferences(context: Context) {
+        val preferences = context.getSharedPreferences(title, Context.MODE_PRIVATE)
         val editor = preferences?.edit()
         editor?.clear()
         editor?.apply()
