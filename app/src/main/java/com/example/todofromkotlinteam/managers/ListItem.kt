e2c72dcbc272bec4e98edc0b152c966cbd1e769e
mariPrikolota <mari.prikolota@yandex.ru>
2021-08-21 20:27:05 +0300

    events №1

--- app/src/main/java/com/example/todofromkotlinteam/managers/ListItem.kt
+++ app/src/main/java/com/example/todofromkotlinteam/managers/ListItem.kt
@@ -1,9 +1,17 @@
 package com.example.todofromkotlinteam.managers
 
 data class ListItem(
+<<<<<<< Updated upstream
     var titleText: String,
     var contentText: String,
     var timeText: String,
     var nameText: String
 )
 
+=======
+    var titleText : String,
+    var contentText : String,
+    var timeText : String,
+    var nameText : String,
+)
+>>>>>>> Stashed changes
