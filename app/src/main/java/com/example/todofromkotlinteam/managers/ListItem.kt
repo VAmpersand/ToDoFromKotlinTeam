6236682d441c6c8e95249f96735702947a269e79
mariPrikolota <mari.prikolota@yandex.ru>
2021-08-21 22:47:51 +0300

    Revert "events №1"
    This reverts commit e2c72dcbc272bec4e98edc0b152c966cbd1e769e.

--- app/src/main/java/com/example/todofromkotlinteam/managers/ListItem.kt
+++ app/src/main/java/com/example/todofromkotlinteam/managers/ListItem.kt
@@ -1,17 +1,9 @@
 package com.example.todofromkotlinteam.managers
 
 data class ListItem(
-<<<<<<< Updated upstream
     var titleText: String,
     var contentText: String,
     var timeText: String,
     var nameText: String
 )
 
-=======
-    var titleText : String,
-    var contentText : String,
-    var timeText : String,
-    var nameText : String,
-)
->>>>>>> Stashed changes
--- app/src/main/java/com/example/todofromkotlinteam/managers/MyAdapter.kt
+++ app/src/main/java/com/example/todofromkotlinteam/managers/MyAdapter.kt
@@ -4,15 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-<<<<<<< Updated upstream
-=======
-import android.widget.ImageView
->>>>>>> Stashed changes
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todofromkotlinteam.R
 
-<<<<<<< Updated upstream
 class MyAdapter(listArray:ArrayList<ListItem>,context: Context): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
     var listArrayR = listArray
     var contextR = context
@@ -28,51 +23,21 @@ class MyAdapter(listArray:ArrayList<ListItem>,context: Context): RecyclerView.Ad
             tvContent.text = listItem.contentText
             tvTime.text = listItem.timeText
             tvInform.text = listItem.nameText
-=======
-class MyAdapter(listArray: ArrayList<ListItem>,context: Context): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
-    var listArrayR = listArray
-    var contextR = context
-
-    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
-        val tvTitle = view.findViewById<TextView>(R.id.titleView)
-        val tvContene = view.findViewById<TextView>(R.id.contentView)
-        val tvTime = view.findViewById<TextView>(R.id.timeView)
-        val tvName = view.findViewById<TextView>(R.id.nameView)
-
-        fun bind(listItem: ListItem, context: Context){
-            tvTitle.text = listItem.titleText
-            tvContene.text = listItem.contentText
-            tvTime.text = listItem.timeText
-            tvName.text = listItem.nameText
->>>>>>> Stashed changes
         }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(contextR)
-<<<<<<< Updated upstream
         return  ViewHolder(inflater.inflate(R.layout.shablon_events, parent,false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         var listItem =listArrayR.get(position)
         holder.bind(listItem, contextR)
-=======
-        return  ViewHolder(inflater.inflate(R.layout.shablon_events,parent,false))
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       var listItem = listArrayR.get(position)
-        holder.bind(listItem,contextR)
->>>>>>> Stashed changes
     }
 
     override fun getItemCount(): Int {
         return listArrayR.size
     }
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 }
