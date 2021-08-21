c7f0ad72e1eb54b576464b320d92c2feefd40f6d
mariPrikolota <mari.prikolota@yandex.ru>
2021-08-21 14:23:53 +0300

    error

--- app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todofromkotlinteam.managers.ListItem
+import com.example.todofromkotlinteam.managers.MyAdapter
 import com.example.todofromkotlinteam.managers.SharedPreferencesKey
 import com.example.todofromkotlinteam.managers.SharedPreferencesManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         showWelcomeActivityIfNeeded()
+
+        val list = ArrayList<ListItem>()
+        list.addAll(fileArras(resources.getStringArray(R.array.title),
+            resources.getStringArray(R.array.content),
+            resources.getStringArray(R.array.time),
+            resources.getStringArray(R.array.name)))
+        recyclerView.hasFixedSize()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(list, this)
     }
 
     private fun showWelcomeActivityIfNeeded() {
@@ -28,5 +41,14 @@ class MainActivity : AppCompatActivity() {
 
     fun onClickRestart(view: View) {
         SharedPreferencesManager(this).resetSharedPreferences()
+    }
+
+    fun fileArras(titleArray:Array<String>, contentArray:Array<String>, timeArray:Array<String>, nameArray:Array<String>):List<ListItem>{
+        var listItemArray = ArrayList<ListItem>()
+        for(n in 0..titleArray.size - 1){
+            var listItem = ListItem(titleArray[n],contentArray[n],timeArray[n],nameArray[n])
+            listItemArray.add(listItem)
+        }
+        return  listItemArray
     }
 }
---
+++ app/src/main/java/com/example/todofromkotlinteam/managers/MyAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.todofromkotlinteam.managers
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todofromkotlinteam.R
+
+class MyAdapter(listArray:ArrayList<ListItem>,context: Context): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
+    var listArrayR = listArray
+    var contextR = context
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view){
+        val tvTitle = view.findViewById<TextView>(R.id.tvTitle)
+        val tvContent = view.findViewById<TextView>(R.id.tvContent)
+        val tvTime = view.findViewById<TextView>(R.id.tvTime)
+        val tvInform = view.findViewById<TextView>(R.id.tvInfom)
+
+        fun  bind(listItem: ListItem, context: Context){
+            tvTitle.text = listItem.titleText
+            tvContent.text = listItem.contentText
+            tvTime.text = listItem.timeText
+            tvInform.text = listItem.nameText
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater = LayoutInflater.from(contextR)
+        return  ViewHolder(inflater.inflate(R.layout.shablon_events, parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        var listItem =listArrayR.get(position)
+        holder.bind(listItem, contextR)
+    }
+
+    override fun getItemCount(): Int {
+        return listArrayR.size
+    }
+}
