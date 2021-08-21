a30e14323f478bb68d0d57ff613a08730b389965
VAmpersand <VP_ampersand_P@icloud.com>
2021-08-21 22:26:29 +0300

    Fixed bugs after merge
    This reverts commit c7f0ad72e1eb54b576464b320d92c2feefd40f6d.

--- app/src/main/java/com/example/todofromkotlinteam/ProfileFragment.kt
+++ app/src/main/java/com/example/todofromkotlinteam/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.todofromkotlinteam
 
+import android.content.Context
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.LayoutInflater
@@ -8,7 +9,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 
-class ProfileFragment : Fragment() {
+class ProfileFragment(context: Context) : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.profile_fragment, container, false)
