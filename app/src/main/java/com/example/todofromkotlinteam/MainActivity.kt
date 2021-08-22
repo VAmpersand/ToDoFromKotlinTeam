f4f71cb61208fbac9d6309a5b703060dfb46ed43
mariPrikolota <mari.prikolota@yandex.ru>
2021-08-22 12:49:17 +0300

    events
    This reverts commit 10ed2cc6927a86de9c05dcba31018b06fd78f38a.

--- app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/MainActivity.kt
@@ -4,8 +4,12 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todofromkotlinteam.managers.ListItem
+import com.example.todofromkotlinteam.managers.MyAdapter
 import com.example.todofromkotlinteam.managers.SharedPreferencesKey
 import com.example.todofromkotlinteam.managers.SharedPreferencesManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         showWelcomeActivityIfNeeded()
+
+        val list = ArrayList<ListItem>()
+        list.add(ListItem("Virtual Doctors’ Appointment","Regular virutal catchup with the doctor…","12:00 - 12:30","Dr. Juliean Adams"))
+        list.add(ListItem("Guided group meditation","Group meditation session at the office ","13:00 - 13:30","Yoga instructor"))
+        list.add(ListItem("Sprint planning ","Catchup to plan for next week’s sprint","14:00-15:00","Dev. Team "))
+        list.add(ListItem("Evening. yoga session","Home yoga session for sleep.","19:00 - 19:30"," Dr. Juliean Adams"))
+        list.add(ListItem("Virtual Doctors’ Appointment","Regular virutal catchup with the doctor…","12:00 - 12:30","Dr. Juliean Adams"))
+        list.add(ListItem("Guided group meditation","Group meditation session at the office ","13:00 - 13:30","Yoga instructor"))
+        list.add(ListItem("Sprint planning ","Catchup to plan for next week’s sprint","14:00-15:00","Dev. Team "))
+        list.add(ListItem("Evening. yoga session","Home yoga session for sleep.","19:00 - 19:30"," Dr. Juliean Adams"))
+        
+        recyclerView.hasFixedSize()
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyAdapter(list, this)
     }
 
     private fun showWelcomeActivityIfNeeded() {
---
+++ app/src/main/java/com/example/todofromkotlinteam/managers/ListItem.kt
@@ -0,0 +1,8 @@
+package com.example.todofromkotlinteam.managers
+
+data class ListItem(
+    var titleText: String,
+    var contentText: String,
+    var timeText: String,
+    var nameText: String
+)
