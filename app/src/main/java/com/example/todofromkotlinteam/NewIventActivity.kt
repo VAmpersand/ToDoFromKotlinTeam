79a566c557999f8c5a89de3ef091f2dadd86a254
Priklll <priklll1989@gmail.com>
2021-09-01 19:47:23 +0300

    new ivent 70%

--- app/src/main/java/com/example/todofromkotlinteam/NewIventActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/NewIventActivity.kt
@@ -2,6 +2,7 @@ package com.example.todofromkotlinteam
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 
 
 class NewIventActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class NewIventActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_event_activity)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.main_orange)
+        window.navigationBarColor = ContextCompat.getColor(this,R.color.main_orange)
     }
 }
