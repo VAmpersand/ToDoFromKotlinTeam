f56ff1e645c189c447abaded6abfed011202cea3
VAmpersand <VP_ampersand_P@icloud.com>
2021-09-14 23:22:01 +0300

    Added AlertDialogFragment

--- app/src/main/java/com/example/todofromkotlinteam/NewEventActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/NewEventActivity.kt
@@ -4,17 +4,16 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
-import com.example.todofromkotlinteam.views.EventDataFieldType
-import com.example.todofromkotlinteam.views.EventType
-import com.example.todofromkotlinteam.views.InputTypeDialogView
-import com.example.todofromkotlinteam.views.InputTypeProjectView
+import com.example.todofromkotlinteam.views.*
 import kotlinx.android.synthetic.main.new_event_additing_layout.*
 import kotlinx.android.synthetic.main.new_event_field_layout.view.*
 import kotlinx.android.synthetic.main.time_input_dialog_layout.view.okButton
 import kotlinx.android.synthetic.main.type_project_dialog_layout.*
 import kotlinx.android.synthetic.main.type_project_dialog_layout.view.*
 
-class NewEventActivity: AppCompatActivity() {
+class NewEventActivity: AppCompatActivity(), OnTypeDialogButtonClickListener {
+    private var currentType: EventType? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_event_additing_layout)
@@ -22,8 +21,13 @@ class NewEventActivity: AppCompatActivity() {
         configureFields()
     }
 
-    fun onClickBack(view : View) { finish() }
-    fun onClickAddEvent(view : View) { finish() }
+    fun onClickBack(view: View) {
+        finish()
+    }
+
+    fun onClickAddEvent(view: View) {
+        finish()
+    }
 
     private fun configureFields() {
         eventNameField?.configureField(EventDataFieldType.NAME)
@@ -32,7 +36,7 @@ class NewEventActivity: AppCompatActivity() {
 
         eventTypeField?.configureField(EventDataFieldType.TYPE)
         eventTypeField?.inputField?.setOnClickListener {
-            showSetTypeDialog()
+            InputTypeDialogView(currentType, this).show(supportFragmentManager, "MyCustomFragment")
         }
 
         eventDateField?.configureField(EventDataFieldType.DATE)
@@ -65,50 +69,6 @@ class NewEventActivity: AppCompatActivity() {
         }
     }
 
-    private fun showSetTypeDialog() {
-//        val view = View.inflate(this, R.layout.type_project_dialog_layout, null)
-//        view.checkPlans?.isChecked = currentType == EventType.PLANS
-//        view.checkIdeas?.isChecked = currentType == EventType.IDEAS
-//
-//        val builder = AlertDialog.Builder(this)
-//        builder.setView(view)
-
-        val dialog = InputTypeDialogView()
-//        dialog.show()
-        dialog.show(supportFragmentManager, "MyCustomFragment")
-
-        dialog.okButton?.setOnClickListener {
-            if (dialog.currentType == EventType.PLANS) {
-                eventTypeField?.inputField?.setText(R.string.plans)
-            } else if (dialog.currentType == EventType.IDEAS) {
-                eventTypeField?.inputField?.setText(R.string.ideas)
-            }
-            dialog.hide()
-        }
-//        dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
-
-//        view.checkPlans?.setOnClickListener {
-//            currentType = EventType.PLANS
-//            view.checkPlans?.isChecked = true
-//            view.checkIdeas?.isChecked = false
-//        }
-//
-//        view.checkIdeas?.setOnClickListener {
-//            currentType = EventType.IDEAS
-//            view.checkPlans?.isChecked = false
-//            view.checkIdeas?.isChecked = true
-//        }
-//
-//        view.okButton?.setOnClickListener {
-//            if (currentType == EventType.PLANS) {
-//                eventTypeField?.inputField?.setText(R.string.plans)
-//            } else if (currentType == EventType.IDEAS) {
-//                eventTypeField?.inputField?.setText(R.string.ideas)
-//            }
-//            dialog.hide()
-//        }
-    }
-
     private fun showSetDataDiolog() {
         val view = InputTypeProjectView(this)
 
@@ -121,6 +81,16 @@ class NewEventActivity: AppCompatActivity() {
 
         view.okButton?.setOnClickListener {
             dialog.hide()
+        }
+    }
+
+    override fun onTypeOkClickListener(type: EventType?) {
+        currentType = type
+
+        if (type == EventType.PLANS) {
+            eventTypeField?.inputField?.setText(R.string.plans)
+        } else if (type == EventType.IDEAS) {
+            eventTypeField?.inputField?.setText(R.string.ideas)
         }
     }
 }
--- app/src/main/java/com/example/todofromkotlinteam/views/InputTypeDialogView.kt
+++ app/src/main/java/com/example/todofromkotlinteam/views/InputTypeDialogView.kt
@@ -8,25 +8,34 @@ import androidx.fragment.app.DialogFragment
 import com.example.todofromkotlinteam.R
 import kotlinx.android.synthetic.main.type_project_dialog_layout.*
 
-class InputTypeDialogView : DialogFragment() {
-    var currentType: EventType? = null
+interface OnTypeDialogButtonClickListener {
+    fun onTypeOkClickListener(type: EventType? = null)
+}
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+class InputTypeDialogView(currentType: EventType?, listener: OnTypeDialogButtonClickListener) : DialogFragment() {
+    private val alertClickListener = listener
+    private var currentType: EventType? = currentType
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dialog?.window?.setBackgroundDrawableResource(android.R.color.transparent)
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
         return inflater.inflate(R.layout.type_project_dialog_layout, container, false)
     }
 
     override fun onStart() {
         super.onStart()
+
+        configureDialogAlert()
         configureListeners()
-        dialog?.window?.setLayout(
-            ViewGroup.LayoutParams.MATCH_PARENT,
-            ViewGroup.LayoutParams.WRAP_CONTENT
-        )
+//        checkPlans?.isChecked = currentType == EventType.PLANS
+//        checkIdeas?.isChecked = currentType == EventType.IDEAS
+
+//        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    private fun configureDialogAlert() {
+        checkPlans?.isChecked = currentType == EventType.PLANS
+        checkIdeas?.isChecked = currentType == EventType.IDEAS
     }
 
     private fun configureListeners() {
@@ -41,9 +50,10 @@ class InputTypeDialogView : DialogFragment() {
             checkPlans?.isChecked = false
             checkIdeas?.isChecked = true
         }
-    }
 
-    fun hide() {
-        dialog?.hide()
+        okButton?.setOnClickListener {
+            alertClickListener.onTypeOkClickListener(currentType)
+            dialog?.hide()
+        }
     }
 }
