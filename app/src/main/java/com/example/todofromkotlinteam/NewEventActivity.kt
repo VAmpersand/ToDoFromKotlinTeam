252acf89a149fb8bf8a3a8acfd4337497c0e8d71
Priklll <priklll1989@gmail.com>
2021-09-14 22:10:46 +0300

    Date select

--- app/src/main/java/com/example/todofromkotlinteam/NewEventActivity.kt
+++ app/src/main/java/com/example/todofromkotlinteam/NewEventActivity.kt
@@ -1,40 +1,50 @@
 package com.example.todofromkotlinteam
 
 import android.annotation.SuppressLint
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.CalendarView
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.todofromkotlinteam.views.EventDataFieldType
 import kotlinx.android.synthetic.main.date_input_dialog_layout.*
 import kotlinx.android.synthetic.main.date_input_dialog_layout.view.*
 import kotlinx.android.synthetic.main.new_event_additing_layout.*
+import kotlinx.android.synthetic.main.new_event_field_layout.*
 import kotlinx.android.synthetic.main.new_event_field_layout.view.*
-import kotlinx.android.synthetic.main.settings_fragment.*
-import kotlinx.android.synthetic.main.settings_fragment.view.*
 import kotlinx.android.synthetic.main.time_input_dialog_layout.view.*
-import java.text.DateFormat
 import java.text.SimpleDateFormat
+import java.time.Month
+import java.time.Year
 import java.util.*
 
 
-class NewEventActivity: AppCompatActivity() {
+class NewEventActivity : AppCompatActivity() {
 
-       override fun onCreate(savedInstanceState: Bundle?) {
+
+    @SuppressLint("NewApi")
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.new_event_additing_layout)
-        configureFields()
 
+        configureFields()
 
     }
 
-    fun onClickBack(view : View) { finish() }
-    fun onClickAddEvent(view : View) { finish() }
 
-    fun onClickOkDate(view: View) {
+    fun onClickBack(view: View) {
+        finish()
+    }
 
+    fun onClickAddEvent(view: View) {
+        finish()
+    }
 
-        }
 
     private fun configureFields() {
         eventNameField?.configureField(EventDataFieldType.NAME)
@@ -48,7 +58,8 @@ class NewEventActivity: AppCompatActivity() {
 
         eventDateField?.configureField(EventDataFieldType.DATE)
         eventDateField?.inputField?.setOnClickListener {
-              showSetDateDialog()
+            showSetDateDialog()
+
 
         }
         eventStartTimeField?.configureField(EventDataFieldType.START_TIME)
@@ -82,15 +93,68 @@ class NewEventActivity: AppCompatActivity() {
         val view = View.inflate(this, R.layout.date_input_dialog_layout, null)
         val builder = AlertDialog.Builder(this)
         builder.setView(view)
-
         val dialog = builder.create()
+//        val calendar = findViewById<CalendarView>(R.id.calendarView)
         dialog.show()
         dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
-
-//        view.okButtonDate?.setOnClickListener {
-//            dialog.hide()
+//        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+//            val correctMonth = month + 1
+//            inputField?.setText("$year-$correctMonth-$dayOfMonth")
 //        }
+        view.okButtonDate?.setOnClickListener {
+
+
+         dialog.hide()
+        }
+
     }
+
+//    private fun showSetDateDialog() {
+//
+//        val view = View.inflate(this, R.layout.date_input_dialog_layout, null)
+//        val builder = AlertDialog.Builder(this)
+//        builder.setView(view)
+//        val dialog = builder.create()
+//        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+//        val editText = EventDataFieldType.DATE as TextView
+//        val calendar = findViewById<CalendarView>(R.id.calendarView)
+//        dialog.show()
+//        dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+//        calendar.setOnDateChangeListener { view1: CalendarView, year: Int, month: Int, dayOfMonth: Int ->
+//            val correctMonth = month + 1
+//            editText.text = "$year-$correctMonth-$dayOfMonth"
+//
+//            view.okButtonDate?.setOnClickListener {
+//
+//
+//                dialog.hide()
+//            }
+//
+//        }
+//    }
+
+//     fun onClickOkDate(view: View){
+//         val calendar = findViewById<CalendarView>(R.id.calendarView)
+//         val editText = EventDataFieldType.DATE as TextView
+//         calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+//             val correctMonth = month + 1
+//
+//             inputField?.setText("$year-$correctMonth-$dayOfMonth")
+//             editText.setText("$year-$correctMonth-$dayOfMonth")
+//         }
+//     }
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/todofromkotlinteam/views/InputDateDialogView.kt
+++ app/src/main/java/com/example/todofromkotlinteam/views/InputDateDialogView.kt
@@ -1,10 +1,12 @@
 package com.example.todofromkotlinteam.views
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.CalendarView
 import android.widget.LinearLayout
-
+import android.widget.Toast
 import com.example.todofromkotlinteam.R
 
 
@@ -14,20 +16,40 @@ class InputDateDialogView: LinearLayout {
     constructor(context: Context?) : super(context)
     constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context,attrs,defStyleAttr)
 
+    @SuppressLint("WrongConstant")
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+
         initializeLayout()
+        configureField()
+//        val calendar = findViewById<CalendarView>(R.id.calendarView)
+//        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+//            Toast.makeText(
+//                context,
+//                "$dayOfMonth/$month/$year",
+//                4000,
+//            ).show()}
 
     }
 
     private fun initializeLayout() {
         val inflater = context?.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-        inflater.inflate(R.layout.date_input_dialog_layout, this)
+        inflater.inflate(com.example.todofromkotlinteam.R.layout.date_input_dialog_layout, this)
+    }
+
+    @SuppressLint("WrongConstant")
+    fun configureField() {
+//        val calendar = findViewById<CalendarView>(R.id.calendarView)
+//        calendar.setOnDateChangeListener { view, year, month, dayOfMonth ->
+//            Toast.makeText(
+//                context,
+//                "$dayOfMonth/$month/$year",
+//                4000,
+//            ).show()
+        }
+
     }
 
 
 
-
-
-}
 
 
