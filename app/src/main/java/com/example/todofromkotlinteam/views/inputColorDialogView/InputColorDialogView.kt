4ecbf10b885a15b52f601565c16ec89e383b6892
VAmpersand <VP_ampersand_P@icloud.com>
2021-10-10 17:43:31 +0700

    Edit ListEventTypeDao

--- app/src/main/java/com/example/todofromkotlinteam/views/inputColorDialogView/InputColorDialogView.kt
+++ app/src/main/java/com/example/todofromkotlinteam/views/inputColorDialogView/InputColorDialogView.kt
@@ -87,10 +87,11 @@ class InputColorDialogView(listener: OnColorDialogButtonClickListener) : DialogF
 
     private fun getAllListEventType() {
         val listEventTypeDao = RoomAppDB.getAppDB(requireContext())?.listEventTypeDao()
-        val insertIndex = 0
-         if(listEventTypeDao?.getAllListEventType()?.isEmpty() == false) events.addAll(insertIndex,listEventTypeDao?.getAllListEventType()!!)
-        rcView?.adapter?.notifyItemInserted(insertIndex)
-
+        if (listEventTypeDao?.getAllListEventType()?.isEmpty() == false) events.addAll(
+            0,
+            listEventTypeDao?.getAllListEventType()!!
+        )
+        rcView?.adapter?.notifyItemInserted(0)
     }
 
 }
